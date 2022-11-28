6a02b75daec005e881ebeb92c4244bb006f9f783
Yuki Kikuchi <yuki-kikuchi@mail.nissan.co.jp>
2022-11-28 11:16:08 +0900

    BottomNavigationBarにSettingsの項目を追加

---
+++ lib/settings_view.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class SettingsScreen extends StatelessWidget {
+  const SettingsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
