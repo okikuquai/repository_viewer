fbf59bd5e9ff607cb2f347d4b6cfcedb2d5e6ba8
Yuki Kikuchi <yuki-kikuchi@mail.nissan.co.jp>
2022-12-01 09:31:08 +0900

    repsitory詳細画面にcollaboratorsのアイコンを表示

--- lib/starred_repositories.dart
+++ lib/starred_repositories.dart
@@ -1,5 +1,1 @@
-import 'graphql/searchRepositoriesInTeam.graphql.dart';
-
-List<Query$searchRepositoriesInTeam$organization$team$repositories$edges$node>
-    starredRepository =
-    <Query$searchRepositoriesInTeam$organization$team$repositories$edges$node>[];
+List<String> favoriteRepositoryIDs = <String>[];
