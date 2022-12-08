c62575f9b9a76e9325e18c1b46bbfde834fd609f
Yuki Kikuchi <yuki-kikuchi@mail.nissan.co.jp>
2022-12-08 17:38:53 +0900

    関数名の変更とpagination機能を追加

--- lib/orgnization_repository_view.dart
+++ lib/orgnization_repository_view.dart
@@ -39,7 +39,7 @@ class _OrganizationRepositoryListHome
                         icon: const Icon(Icons.groups))
                   ],
                 ),
-                body: OrganizationRepositoryCardList(
+                body: OrganizationRepositoryBody(
                   orgName: orgName,
                 ));
           }
@@ -48,19 +48,19 @@ class _OrganizationRepositoryListHome
   }
 }
 
-class OrganizationRepositoryCardList extends HookConsumerWidget {
-  const OrganizationRepositoryCardList({super.key, required this.orgName});
+class OrganizationRepositoryBody extends HookConsumerWidget {
+  const OrganizationRepositoryBody({super.key, required this.orgName});
   final String orgName;
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final qryResult = useQuery$getRepositoriesInOrganization(
         Options$Query$getRepositoriesInOrganization(
             variables: Variables$Query$getRepositoriesInOrganization(
-                orgName: orgName, first: 100)));
+                orgName: orgName, first: 15)));
 
     //ロード完了していない場合
     if (qryResult.result.isLoading) {
-      return loadingAnimation();
+      // return loadingAnimation();
     }
     //例外スローした場合
     else if (qryResult.result.hasException) {
@@ -69,29 +69,33 @@ class OrganizationRepositoryCardList extends HookConsumerWidget {
 
     if (qryResult.result.parsedData?.organization?.repositories.edges != null) {
       //ハートの状態を変えるためにStatefulWidgetでリポジトリのリストを表示させる
-      return FavoriteCardList(qryResult: qryResult, orgName: orgName);
+      return OrganizationRepositoryCardList(
+          qryResult: qryResult, orgName: orgName);
     }
     return const Text("no Teams in this Organization");
   }
 }
 
-class FavoriteCardList extends StatefulWidget {
-  const FavoriteCardList(
+class OrganizationRepositoryCardList extends StatefulWidget {
+  const OrganizationRepositoryCardList(
       {Key? key, required this.qryResult, required this.orgName})
       : super(key: key);
   final QueryHookResult<Query$getRepositoriesInOrganization> qryResult;
   final String orgName;
 
   @override
-  createState() => _FavoriteCardList();
+  createState() => _OrganizationRepositoryCardList();
 }
 
-class _FavoriteCardList extends State<FavoriteCardList> {
+class _OrganizationRepositoryCardList
+    extends State<OrganizationRepositoryCardList> {
   @override
   Widget build(BuildContext context) {
     //nullじゃないことが確定しているので!を使う
     final repositories =
         widget.qryResult.result.parsedData!.organization!.repositories.edges!;
+    final pageinfo =
+        widget.qryResult.result.parsedData!.organization!.repositories.pageInfo;
     final repositoriesCount = repositories.length;
     var isFavorite = List.generate(
         repositoriesCount,
@@ -105,50 +109,84 @@ class _FavoriteCardList extends State<FavoriteCardList> {
       });
     }
 
-    return ListView.builder(
-        itemCount: repositoriesCount,
-        itemBuilder: (context, index) {
-          final TextTheme textTheme = Theme.of(context).textTheme;
-          final repository = repositories[index]!.node!;
-          return Card(
-            child: ListTile(
-                trailing: GestureDetector(
-                  child: Icon(
-                      isFavorite[index]
-                          ? Icons.favorite
-                          : Icons.favorite_border,
-                      color: isFavorite[index] ? Colors.red : null),
-                  onTap: () {
-                    setFavorite(index);
-                    if (isFavorite[index]) {
-                      FavoriteRepositories.value
-                          .removeWhere((element) => element == repository.id);
-                    } else {
-                      FavoriteRepositories.addItem(repository.id);
-                    }
-                  },
-                ),
-                title: Text(
-                  repository.name,
-                  style: textTheme.headline5,
-                  maxLines: 1,
-                  overflow: TextOverflow.ellipsis,
-                ),
-                subtitle: Text(
-                  repository.description ?? "no description",
-                  maxLines: 1,
-                  overflow: TextOverflow.ellipsis,
-                ),
-                onTap: () async {
-                  await Navigator.of(context).push(
-                    MaterialPageRoute(
-                      builder: (context) =>
-                          RepositoryView(repositoryID: repository.id),
+    return NotificationListener<ScrollNotification>(
+        onNotification: (ScrollNotification scrollNotification) {
+          if (scrollNotification is ScrollEndNotification) {
+            final before = scrollNotification.metrics.extentBefore;
+            final max = scrollNotification.metrics.maxScrollExtent;
+            //下端に移動しきったか
+            if (before == max) {
+              //取得したコンテンツが最後のコンテンツではないか
+              if (!pageinfo.hasNextPage) return false;
+              //さらにリポジトリを取得
+              widget.qryResult.fetchMore(
+                  FetchMoreOptions$Query$getRepositoriesInOrganization(
+                      variables: Variables$Query$getRepositoriesInOrganization(
+                          orgName: widget.orgName,
+                          first: 15,
+                          after: pageinfo.endCursor),
+                      updateQuery: (previousResultData, fetchMoreResultData) {
+                        final List<dynamic> items = <dynamic>[
+                          ...previousResultData?["organization"]["repositories"]
+                                  ["edges"] as List<dynamic>? ??
+                              <dynamic>[],
+                          ...fetchMoreResultData?["organization"]
+                                  ["repositories"]["edges"] as List<dynamic>? ??
+                              <dynamic>[],
+                        ];
+                        fetchMoreResultData?["organization"]["repositories"]
+                            ["edges"] = items;
+                        return fetchMoreResultData;
+                      }));
+            }
+            return true;
+          }
+          return false;
+        },
+        child: ListView.builder(
+            itemCount: repositoriesCount,
+            itemBuilder: (context, index) {
+              final TextTheme textTheme = Theme.of(context).textTheme;
+              final repository = repositories[index]!.node!;
+              return Card(
+                child: ListTile(
+                    trailing: GestureDetector(
+                      child: Icon(
+                          isFavorite[index]
+                              ? Icons.favorite
+                              : Icons.favorite_border,
+                          color: isFavorite[index] ? Colors.red : null),
+                      onTap: () {
+                        setFavorite(index);
+                        if (isFavorite[index]) {
+                          FavoriteRepositories.value.removeWhere(
+                              (element) => element == repository.id);
+                        } else {
+                          FavoriteRepositories.addItem(repository.id);
+                        }
+                      },
                     ),
-                  );
-                  setState(() {});
-                }),
-          );
-        });
+                    title: Text(
+                      repository.name,
+                      style: textTheme.headline5,
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                    subtitle: Text(
+                      repository.description ?? "no description",
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                    onTap: () async {
+                      await Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: (context) =>
+                              RepositoryView(repositoryID: repository.id),
+                        ),
+                      );
+                      setState(() {});
+                    }),
+              );
+            }));
   }
 }
