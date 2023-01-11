b00ab1c2e6dbb51b6438d135bdcc27b88952c825
Yuki Kikuchi <yuki-kikuchi@mail.nissan.co.jp>
2023-01-11 16:59:25 +0900

    コメント追加

--- lib/client.dart
+++ lib/client.dart
@@ -1,6 +1,7 @@
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
+//String.fromEnviromentはconstにしないと正しく読み取らない
 final githubTokenProvider = StateNotifierProvider<GithubSetting, String>(
   (ref) => GithubSetting(
       defaultValue: const String.fromEnvironment("Github_DefaultToken"),
