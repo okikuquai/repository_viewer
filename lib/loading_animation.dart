3227d48c62f7cf09f9937928cdef3aa729634b2d
Yuki Kikuchi <yuki-kikuchi@mail.nissan.co.jp>
2023-01-18 10:14:54 +0900

    functionからclassに変更

--- lib/loading_animation.dart
+++ lib/loading_animation.dart
@@ -1,40 +1,50 @@
 import 'package:flutter/material.dart';
 import 'package:loading_animation_widget/loading_animation_widget.dart';
 
-Widget loadingAnimation() {
-  return Center(
-      child: Column(
-    mainAxisSize: MainAxisSize.min,
-    children: [
-      LoadingAnimationWidget.hexagonDots(
-        color: Colors.black45,
-        size: 50,
-      ),
-      const Padding(
-        padding: EdgeInsets.all(15),
-        child: Text('Loading ...'),
-      )
-    ],
-  ));
+class LoadingAnimation extends StatelessWidget {
+  const LoadingAnimation({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            LoadingAnimationWidget.hexagonDots(
+              color: Colors.black45,
+              size: 50,
+            ),
+            const Padding(
+              padding: EdgeInsets.all(15),
+              child: Text('Loading ...'),
+            )
+          ],
+        ));
+  }
 }
 
-Widget loadingAnimationWithAppbar() {
-  return Scaffold(
-      appBar: AppBar(
-        title: const Text('loading'),
-      ),
-      body: Center(
-          child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          LoadingAnimationWidget.hexagonDots(
-            color: Colors.black45,
-            size: 50,
-          ),
-          const Padding(
-            padding: EdgeInsets.all(15),
-            child: Text('Loading ...'),
-          )
-        ],
-      )));
+class LoadingAnimationWithAppbar extends StatelessWidget {
+  const LoadingAnimationWithAppbar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('loading'),
+        ),
+        body: Center(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                LoadingAnimationWidget.hexagonDots(
+                  color: Colors.black45,
+                  size: 50,
+                ),
+                const Padding(
+                  padding: EdgeInsets.all(15),
+                  child: Text('Loading ...'),
+                )
+              ],
+            )));
+  }
 }
